be73a1f9a3defa583d4d30547fa39d897ade1a59
Ramon Garcia <ramongarciaalarcia@gmail.com>
2016-04-22 11:36:32 +0200

    Primera versió de la funció direct_cost.m

---
+++ direct_cost.m
@@ -0,0 +1,17 @@
+function [weight] = direct_cost(g, src_index, dst_index)
+
+    i = 1;
+    weight = -1; % If the weight is not found, the function must return -1
+    
+    while (i <= length(g.segments) && weight ~= -1)
+        
+        % If this is the segment we were looking for
+        if (g.segments(i).source == src_index && g.segments(i).destination == dst_index)
+            weight = g.segments(i).weight; % Return the weight of this segment
+        end
+        i = i + 1;
+        
+    end
+
+
+end
