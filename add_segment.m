112a818977ef7acf22564755e95eaf3584b73e4b
Pere Fullana Bosch <Pere Fullana Bosch>
2016-04-23 15:15:14 +0200

    ara si

--- add_segment.m
+++ add_segment.m
@@ -1,6 +1,6 @@
 function [g2] = add_segment(g1,segment)
  g2=g1;
- if (seg.source~<0 || seg.destination ~<0 || seg.weight~>0)
+ if (seg.source<0 || seg.destination<0 || seg.weight<0)
   fprintf('there are an error with something');
  else
   k=length (g1)+1;
