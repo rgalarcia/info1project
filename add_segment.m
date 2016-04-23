0d0b9d210d1a699c210d5eeeb420e13c30952577
Pere Fullana Bosch <Pere Fullana Bosch>
2016-04-23 15:13:01 +0200

    He arreglat el que fallava crec

--- add_segment.m
+++ add_segment.m
@@ -1,14 +1,9 @@
 function [g2] = add_segment(g1,segment)
-%Description: Given a graph g1 and a segment, returns a new graph
-%g2 with the new segment.
-%If the segment is not correct (non existing nodes or zero/negative distance) then an error is
-%shown in console and the returned g2 is equal to g1.
  g2=g1;
- if (node_name=='ERROR' || segment.w<=0)
+ if (seg.source~<0 || seg.destination ~<0 || seg.weight~>0)
   fprintf('there are an error with something');
-  end
  else
-  k=length (g1);
-  g2(k+1)=(segment);
+  k=length (g1)+1;
+  g2(k)=(segment);
   end
 end
