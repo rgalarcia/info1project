7441be4c8c6b338393afcede059fd855ef27fce6
Ramon Garcia <ramongarciaalarcia@gmail.com>
2016-04-28 09:03:39 +0200

    create_segment.m: Invalid weight now <=0 instead of <0.

--- node_index.m
+++ node_index.m
@@ -1,6 +1,6 @@
 % Given a node, this functions tries to find whether it is present in a
 % specified graph, and if so, return the index where it can be found
-function [index] = node_index (g,node_name)
+function [index] = node_index (g, node_name)
 
     index = -1; % If it can not be found, returns -1.
     i = 1;
