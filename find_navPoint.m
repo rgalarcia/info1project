53a06142e22d4fc16a31b5aca37f5da35ec778c4
Estel Ferrer Torres <Estel Ferrer Torres>
2016-05-01 12:58:04 +0200

    -Pujut les dos funcions que vaig fer divendres (airport_sides & find_navPoint) -He modificat read_airports, read_navPoints i locate_airports.

---
+++ find_navPoint.m
@@ -0,0 +1,22 @@
+%This function looks for the navigation point with name navPoint_name and 
+%returns its location and identifier. 
+%If error, a message is shown and the returned  values are -1.
+function [lat, lon, id]=find_navPoint(NP,navPoint_name)
+lat=-1; %Si no els troba romana aquest valor. 
+lon=-1; %Si els troba canvien. 
+id=-1;
+i=1;
+found=false;
+while (found==false && i<=length(NP))
+ if navPoint_name=NP(i).NavPointName
+     lat=NP(i).lat;   %Ara canvien. 
+     lon=NP(i).long;
+     id=NP(i).NavPointID;
+     found=true;
+ end
+ i=i+1;
+end 
+if found==false
+    fprintf('error')
+end 
+end 
