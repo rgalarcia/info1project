1b04372464055c9a235ab1ed10d0a20ff12cd6fc
EstelFerrer <estelferrer9@gmail.com>
2016-05-02 21:26:54 +0200

    Intento fer un replace

--- locate_airports.m
+++ locate_airports.m
@@ -1,19 +1,27 @@
-function Airports_full = locate_airports(Airports,NP)
-    n = 1;
-    while (n<=length(Airports))
-        i = 1;
-        while (i<=length(Airports(n).SIDS))
-            [lat,lon,id] = find_navPoint(NP,Airports(n).SIDS(i).NavPointName);
-            Airports(n).SIDS(i).long = lon;
-            Airports(n).SIDS(i).lat = lat;
-            Airports(n).SIDS(i).NavPointID = id;
-            i = i + 1;
-        end
-        Airports(n).long = Airports(n).SIDS(1).long;
-        Airports(n).lat = Airports(n).SIDS(1).lat;
-        n = n + 1;
+function Airports_full=locate_airports(Airports,NP)
+    n=1;
+while (n<=length(Airports))
+    i=1;
+    while (i<=length(Airports(n).SIDS))
+        [lat,lon,id]=find_navPoint(NP,Airports(n).SIDS(i).NavPointName);
+        Airports(n).SIDS(i).long=lon;
+        Airports(n).SIDS(i).lat=lat;
+        Airports(n).SIDS(i).NavPointID=id;
+        i=i+1;
     end
-    Airports_full = Airports;
+    i=1;
+    while (i<=length(Airports(n).STARTS))
+        [lat,lon,id]=find_navPoint(NP,Airports(n).STARTS(i).NavPointName);
+        Airports(n).STARTS(i).long=lon;
+        Airports(n).STARTS(i).lat=lat;
+        Airports(n).STARTS(i).NavPointID=id;
+        i=i+1;
+    end
+    Airports(n).long=Airports(n).SIDS(1).long;
+    Airports(n).lat=Airports(n).SIDS(1).lat;
+    n=n+1
+end
+Airports_full=Airports;
 end 
 
     
