6451345f05e4fe051c0445dcfb0921750556e4c1
Ramon Garcia <ramongarciaalarcia@gmail.com>
2016-05-05 09:31:14 +0200

    Adding comments, making it harder, better, stronger.

--- airport_sids.m
+++ airport_sids.m
@@ -1,10 +1,15 @@
+% Given an airport name, this function looks for its SIDS
+% procedures in an Airports structure and returns the list
 
 function [sids] = airport_sids(Airports, airport_name)
     i = 1;
+    
     while (i <= length(Airports))
-        if (strcmp(Airports(i).name,airport_name))
+        
+        if (strcmp(Airports(i).name, airport_name))
             sids = Airports(i).SIDS;
         end   
+        
         i = i + 1;
     end
 end 
