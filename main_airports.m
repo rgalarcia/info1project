e4907badd186d1f1772d79ff7113a062bfcd0eb0
Pere Fullana Bosch <Pere Fullana Bosch>
2016-05-05 08:40:02 +0200

    Mainacfas
    Signed-off-by: Pere Fullana Bosch <Pere Fullana Bosch>

--- main_airports.m
+++ main_airports.m
@@ -1,5 +1,22 @@
 Airports = read_airports();
+if Airports==[]
+   disp ('Im an error')
+end
 
 NavPoints = read_navPoints();
+if NavPoints==[]
+  disp('Im a error')
+end
 
+Airports=locate_airports(Airports,NavPoints)
 
+barcelona.sids=airport_sids(Airports,LEBL
+
+barcelona.stars=airport_stars(Airports,LEBL)
+
+if barcelona.sids==[]
+  disp('im an error')
+end
+if barcelona.stars==[]
+  disp('Im an error')
+end
