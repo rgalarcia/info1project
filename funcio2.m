801c24a6267baac374c9b579fb7518335b8fc507
Pere Fullana Bosch <Pere Fullana Bosch>
2016-05-13 09:47:23 +0200

    La funcio 2 Signed-off-by: Pere Fullana Bosch <Pere Fullana Bosch>

---
+++ funcio2.m
@@ -0,0 +1,19 @@
+function[g2]=complete_graph(g,airports)
+
+    i=length(g.nodes) + 1; %we need to kwnow how many nodes are
+    
+    n=1;
+    
+    while (n<= length (Airports))
+      g.nodes(i).name=Airports(n).name
+      g.nodes(i).latitude=Airports(n).latitude
+      g.nodes(i).long=Airports(n).long
+      g.nodes(i).type='airport'
+      n = n + 1;
+      i = i + 1;
+      
+    end
+    n = 1;
+    
+    i=lenth (g.segments)+ 1
+    whilen <=length(Airport)
